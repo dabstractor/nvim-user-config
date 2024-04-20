ac8c596a2eac15eb5521b4b4e6778adbc5c4186d
Dustin Schultz <dustindschultz@gmail.com>
2024-04-19 22:43:06 -0400

    added basic user config files

---
+++ config.lua
@@ -0,0 +1,4 @@
+-- Sync clipboard between OS and Neovim.
+--  Uncomment this option if you want your OS clipboard to sync with Neovim.
+--  See `:help 'clipboard'`
+-- vim.opt.clipboard = 'unnamedplus'
