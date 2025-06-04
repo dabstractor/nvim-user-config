5ae809f44927502e0aec51e5988c6b8749a7566f
Dustin Schultz <dustindschultz@gmail.com>
2025-06-04 13:04:23 -0400

    added sidescrolling and cleaned up unused keymaps

--- autocmd.lua
+++ autocmd.lua
@@ -1,1 +1,15 @@
-vim.g.set_scroll(5)
+-- vim.defer_fn(function()
+--   vim.g.set_scroll(10)
+-- end, 4000)
+
+vim.cmd [[
+  set scroll=10
+]]
+vim.defer_fn(function()
+  vim.cmd('redraw')
+end, 1000
+)
+
+vim.cmd [[
+  set sidescroll=1
+]]
--- keymaps.lua
+++ keymaps.lua
@@ -1,12 +1,3 @@
 -- add your personal keymaps here
 
 local map = require 'utils.map'
-
-map({ 'n', 'i', 'v' }, '<ScrollWheelUp>', '<C-u>', { desc = 'Scroll up', silent = true })
-map({ 'n', 'i', 'v' }, '<ScrollWheelDown>', '<C-d>', { desc = 'Scroll down', silent = true })
-map({ 'n', 'i', 'v' }, '<ScrollWheelLeft>', 'zH', { desc = 'Scroll left', silent = true })
-map({ 'n', 'i', 'v' }, '<ScrollWheelRight>', 'zL', { desc = 'Scroll right', silent = true })
-map({ 'n', 'i', 'v' }, '<S-ScrollWheelUp>', '{', { desc = 'Scroll up to whitespace', silent = true })
-map({ 'n', 'i', 'v' }, '<S-ScrollWheelDown>', '}', { desc = 'Scroll down to whitespace', silent = true })
-map({ 'n', 'i', 'v' }, '<Mouse4>', '<cmd>BufSurfBack<cr>', { desc = 'Navigate back', silent = true })
-map({ 'n', 'i', 'v' }, '<Mouse5>', '<cmd>BufSurfForward<cr>', { desc = 'Navigate forward', silent = true })
